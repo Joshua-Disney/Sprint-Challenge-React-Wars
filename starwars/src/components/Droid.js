42608e23014a5ae65345ef2ab38af249f86c1fdd
joshua-disney <joshua.a.disney@gmail.com>
2019-01-11 15:51:16 -0700

    Added new fetch requests.

--- starwars/src/components/Droid.js
+++ starwars/src/components/Droid.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import './StarWars.css';
 
-const Droid = (props) => {
-  return (
-    <div className='droid'> 
-      <h1>My name is {props.character.name}.</h1>
-      <p>I was built on {props.character.homeworld} in {props.character.birth_year}</p>
-      <p>I weigh {props.character.mass} units.</p>
-    </div>
-  )
+class Droid extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      films: [],
+    }
+  }
+
+  componentDidMount() {
+    this.props.character.films.map(film => this.getFilm(film));
+  }
+
+  getFilm = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ films: [...this.state.films, data.title] });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className='droid'> 
+        <h1>I am {this.props.character.name}.</h1>
+        <p>I was built on {this.props.character.homeworld} in {this.props.character.birth_year}</p>
+        <p>I weigh {this.props.character.mass} units.</p>
+        <p>I was in these movies: <ul>{this.state.films.map((film) => {
+          return <li>{film}</li>
+        })}</ul></p>
+      </div>
+    )
+  }
 }
 
 export default Droid;
--- starwars/src/components/Human.js
+++ starwars/src/components/Human.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import './StarWars.css';
 
-const Human = (props) => {
-  return (
-    <div className='human'> 
-      <h1>My name is {props.character.name}.</h1>
-      <p>I was born on {props.character.homeworld} in {props.character.birth_year}</p>
-      <p>I weigh {props.character.mass} units and I've got {props.character.hair_color} hair and {props.character.eye_color} eyes.</p>
+class Human extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      vehicles: [],
+    }
+  }
+  componentDidMount() {
+    this.props.character.vehicles.map(vehicle => this.getVehicle(vehicle));
+  }
+
+  getVehicle = URL => {
+    fetch(URL)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        this.setState({ vehicles: [...this.state.vehicles, data.name] });
+      })
+      .catch(err => {
+        throw new Error(err);
+      });
+  };
+
+  render() {
+    return (
+      <div className='human'> 
+      <h1>My name is {this.props.character.name}.</h1>
+      <p>I was born on {this.props.character.homeworld} in {this.props.character.birth_year}</p>
+      <p>I weigh {this.props.character.mass} units and I've got {this.props.character.hair_color} hair 
+      and {this.props.character.eye_color} eyes.</p>
+      {this.state.vehicles.length !== 0 && <p>I have flown these vehicles: <ul>{this.state.vehicles.map((vehicle) => {
+        return <li>{vehicle}</li>
+      })}</ul></p>}
     </div>
-  )
+    )
+  }
 }
 
 export default Human;
